d3643b5a86fc03f39d44307cea37f08ba2f4fd5c
jack.li <ie_noob@163.com>
2020-07-21 22:23:00 +0800

    构建霍夫曼树

--- nlp/word2vec.py
+++ nlp/word2vec.py
@@ -2,6 +2,7 @@
     word2vec
 """
 import re
+import heapq
 data = """
 
    I have a good friend, and her name is Li Hua. We have become friends for about two years. 
@@ -14,9 +15,9 @@ data_list = re.split(r"[^0-9a-z\S]", data)
 
 word_count = {}
 
-for word in word_count:
+for word in data_list:
     if word:
-        word_count[word] = word_count.get(word, 0)
+        word_count[word] = word_count.get(word, 0) + 1
 
 
 class BinaryTree(object):
@@ -24,11 +25,44 @@ class BinaryTree(object):
     def __init__(self):
         self.left = None
         self.right = None
+        self.value = []
 
 
-word_list = [(k, v) for k, v in word_count.items()]
+word_list = [(v, k) for k, v in word_count.items()]
+word2id = {k1:i for i, (k1, v1) in enumerate(word_list)}
 
 word_list.sort(key=lambda x: x[1])
+print(word_list)
+print(len(word_list))
+
+# goujian huofumanshu
+input_q = word_list
+
+heapq.heapify(input_q)
+
+while len(input_q) > 1:
+    p1, v1 = heapq.heappop(input_q)
+    p2, v2 = heapq.heappop(input_q)
+    if isinstance(v1, str):
+        tree = BinaryTree()
+        tree.value.append(v1)
+
+        v1 = tree
+    if isinstance(v2, str):
+        tree = BinaryTree()
+        tree.value.append(v2)
+
+        v2 = tree
+
+    tree = BinaryTree()
+    tree.left = v1
+    tree.right = v2
+    tree.value = v1.value + v2.value
+
+    input_q.append((p1+p2, tree))
+
+
+
 
 class HuffmanTree(object):
 
