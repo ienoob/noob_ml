34868afc9634c540b070496af5cad3b5407b4a8f
jack.li <ie_noob@163.com>
2020-07-24 00:32:25 +0800

    get tree path

--- nlp/word2vec.py
+++ nlp/word2vec.py
@@ -25,13 +25,15 @@ class BinaryTree(object):
     def __init__(self):
         self.left = None
         self.right = None
+        self.left_q = []
         self.value = []
 
 
 word_list = [(v, k) for k, v in word_count.items()]
-word2id = {k1:i for i, (k1, v1) in enumerate(word_list)}
+word2id = {k1: i for i, (v1, k1) in enumerate(word_list)}
 
-word_list.sort(key=lambda x: x[1])
+word_list = [(v, word2id[k], word2id[k]) for v, k in word_list]
+word_list.sort(key=lambda x: x[0])
 print(word_list)
 print(len(word_list))
 
@@ -41,14 +43,14 @@ input_q = word_list
 heapq.heapify(input_q)
 
 while len(input_q) > 1:
-    p1, v1 = heapq.heappop(input_q)
-    p2, v2 = heapq.heappop(input_q)
-    if isinstance(v1, str):
+    p1, n1, v1 = heapq.heappop(input_q)
+    p2, n2, v2 = heapq.heappop(input_q)
+    if isinstance(v1, int):
         tree = BinaryTree()
         tree.value.append(v1)
 
         v1 = tree
-    if isinstance(v2, str):
+    if isinstance(v2, int):
         tree = BinaryTree()
         tree.value.append(v2)
 
@@ -58,16 +60,29 @@ while len(input_q) > 1:
     tree.left = v1
     tree.right = v2
     tree.value = v1.value + v2.value
+    tree.left_q = v1.value
 
-    input_q.append((p1+p2, tree))
+    input_q.append((p1+p2, n1, tree))
 
+x, y, hft = input_q[0]
+
+
+def f(input_id, input_tree, path):
+    if len(input_tree.left_q) == 0 and input_id in tree.value:
+        return path
+    if input_id in input_tree.left_q:
+        return f(input_id, input_tree.left, path+[0])
+    else:
+        return f(input_id, input_tree.right, path+[1])
+
+
+paht = f(3, hft, [])
+print(paht)
 
 
 
 class HuffmanTree(object):
-
     def __init__(self):
-
 
         self
 
